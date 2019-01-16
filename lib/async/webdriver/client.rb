cc10f9ab137d4a5a40cb9a30358f8e97eb0c46ba
Maksym Pugach <pugach.m@gmail.com>
2019-01-16 14:15:30 +0200

    pass down desired_capabilities

--- lib/async/webdriver/client.rb
+++ lib/async/webdriver/client.rb
@@ -3,12 +3,13 @@ require "async/queue"
 module Async
   module Webdriver
     class Client
-      def initialize(endpoint:)
+      def initialize(endpoint:, desired_capabilities: {})
         @connection = Connection.new endpoint: endpoint
+        @desired_capabilities = desired_capabilities
       end
 
       def session
-        SessionCreator.new connection: @connection
+        SessionCreator.new connection: @connection, desired_capabilities: @desired_capabilities
       end
 
       def status
--- lib/async/webdriver/session_creator.rb
+++ lib/async/webdriver/session_creator.rb
@@ -3,14 +3,17 @@ require "async/queue"
 module Async
   module Webdriver
     class SessionCreator
-      def initialize(connection:)
+      def initialize(connection:, desired_capabilities: {})
         @connection = connection
+        @desired_capabilities = desired_capabilities
       end
 
       def create!
-        value = @connection.call method: "post", path: "session", body: {
-          desiredCapabilities: {}
-        }
+        value = @connection.call(
+          method: "post",
+          path: "session",
+          body: { desiredCapabilities: @desired_capabilities }
+        )
 
         Session.new json: value, connection: @connection
       end
